4b7390268c2ec902ca640a06468f84c3edb24717
Derek Straka <derek@starlab.io>
2015-09-29 15:28:41 -0400

    Updates for SSTATE plus proxy configuration {closes #1}

--- bitbake.sh
+++ bitbake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 setuser builduser \
-	bash -c "source oe-init-build-env && bitbake $*"
+	bash -c "source oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} SSTATE_DIR SSTATE_MIRRORS\" && bitbake $*"
