8f3e51a9bcb1d037311dbe150ed441fceb45041c
Derek Straka <derek@starlab.io>
2015-12-24 13:26:59 -0500

    Add MACHINE to the whitelisted variables

--- bitbake.sh
+++ bitbake.sh
@@ -3,4 +3,4 @@
 cmd=$(basename $0)
 
 setuser builduser \
-	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
+	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} MACHINE SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
