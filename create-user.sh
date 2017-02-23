23019feca1a1a2578cad45fd69ab764072bd0e66
Doug Goldstein <cardoe@cardoe.com>
2017-02-23 11:17:23 -0800

    improve user creation to better detect uid/gid
    Determine the uid/gid from the supplied directory but also fall back to
    an alternate for Docker for Mac to ensure we don't use 0/0.

--- create-user.sh
+++ create-user.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# The user can supply a UID and GID from the docker run command by:
-# docker run -e BUILD_UID=$(id -u) -e BUILD_GID=$(id -g) ...
+homedir=/var/build
+
+# figure out the uid/gid we need to use by integrating the path that has
+# been bind mounted in. this is then used for the builduser.
+# the fallback is 580/580 which happily mapped properly under Docker
+# for Mac back to my real uid/gid.
+BUILD_UID=$(stat --printf=%u ${homedir} 2>&1 > /dev/null)
+BUILD_GID=$(stat --printf=%g ${homedir} 2>&1 > /dev/null)
 BUILD_UID=${BUILD_UID:-580}
 BUILD_GID=${BUILD_GID:-580}
+BUILD_UID=${BUILD_UID/#0/580}
+BUILD_GID=${BUILD_GID/#0/580}
 
 # create a group
 groupadd --gid ${BUILD_GID} --non-unique buildgroup
 
 # create a non-root user
-useradd --no-create-home --home-dir /var/build -s /bin/bash \
+useradd --no-create-home --home-dir ${homedir} -s /bin/bash \
 	--non-unique --uid ${BUILD_UID} --gid ${BUILD_GID} --groups sudo \
 	builduser
 
