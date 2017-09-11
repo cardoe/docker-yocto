f5ccaae399b55e6e8f22a53e8d95f05d2fcc2d2f
Doug Goldstein <cardoe@cardoe.com>
2017-09-11 15:37:37 -0500

    update comments and path for Docker registry

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@
 # ./build.sh TAG NAME
 # e.g. ./build.sh
 # e.g. ./build.sh latest
-# e.g. ./build.sh latest rust
-# all 3 are equal if run from a directory called 'docker-rust'
+# e.g. ./build.sh latest yocto
+# all 3 are equal if run from a directory called 'docker-yocto'
 
 # Try to figure out the project name
 NAME=$(basename $(dirname $(readlink -f $0)))
 NAME=${NAME#docker-}
 NAME=${2:-${NAME}}
 TAG=${1:-latest}
-REGISTRY=registry.star.lab
+REGISTRY=cardoe
 COMPLETE=${REGISTRY}/${NAME}:${TAG}
 
 docker build --rm=true -t ${COMPLETE} .
